7e29a7110b8e93e23f0058b30572018e0672ccd0
AnaClaraazz <anaclaraazzevedo2022@gmail.com>
2022-11-10 10:23:22 -0300

    “Deploy”

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,26 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { Postagem } from './postagem/entitys/postagem.entitys';
 import { PostagemModule } from './postagem/postagem.module';
+import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
+import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: 'root',
-    //   database: 'db_blogpessoal',
-    //   entities: [Postagem, Tema, Usuario],
-    //   synchronize: true
-    // }),
+    /* TypeOrmModule.forRoot({
+       type: 'mysql',
+       host: 'localhost',
+       port: 3306,
+       username: 'root',
+       password: 'root',
+       database: 'db_blogpessoal',
+       entities: [Postagem, Tema, Usuario],
+       synchronize: true
+     }),*/
 
     TypeOrmModule.forRoot({
       type: 'postgres',
